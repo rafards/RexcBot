3183f73dfa5fbcff12243278a53299fc3b57ca6e
Rexcy Mosses <50251783+rafards@users.noreply.github.com>
2026-03-06 10:43:56 +0700

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,1 +1,15 @@
+const bracketCommand = require("../features/bracket/commands/bracketCommand");
 
+module.exports = (client) => {
+
+  client.on("messageCreate", async (message) => {
+
+    if (message.author.bot) return;
+
+    if (message.content === "!bracket") {
+      return bracketCommand.execute(message, client);
+    }
+
+  });
+
+};
