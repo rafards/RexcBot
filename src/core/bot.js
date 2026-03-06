a6efdef2abe86e00d3869799022be9164a720570
Rexcy Mosses <50251783+rafards@users.noreply.github.com>
2026-03-06 10:36:28 +0700

    Update bot.js

--- src/core/bot.js
+++ src/core/bot.js
@@ -1,1 +1,18 @@
+const { Client, GatewayIntentBits } = require("discord.js");
 
+function createBot() {
+
+  const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMembers,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent
+    ]
+  });
+
+  return client;
+
+}
+
+module.exports = createBot;
