544072e7c085eade474535f74b12f816b8bd4b3b
Rexcy Mosses <50251783+rafards@users.noreply.github.com>
2026-03-06 10:51:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-require("dotenv").config();
-const { Client, GatewayIntentBits } = require("discord.js");
+const createBot = require("./src/core/bot");
+const config = require("./src/core/config");
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
+const messageEvent = require("./src/events/messageCreate");
+const interactionEvent = require("./src/events/interactionCreate");
+
+const nicknameFeature = require("./src/features/nickname");
+
+const client = createBot();
+
+messageEvent(client);
+interactionEvent(client);
+
+nicknameFeature(client);
+
+client.once("ready", () => {
+
+  console.log(`Bot Online: ${client.user.tag}`);
+
 });
 
-client.once("clientReady", () => {
-  console.log(`🚀 Enterprise System Online: ${client.user.tag}`);
-});
-
-/* ================= LOAD SYSTEMS ================= */
-
-require("./systems/nicknameSystem")(client);
-require("./systems/bracketSystem")(client);
-
-/* ================================================= */
-
-client.login(process.env.TOKEN);
+client.login(config.TOKEN);
