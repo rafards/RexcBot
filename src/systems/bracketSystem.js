fc5733d9efcf12444a81fe87a528aafe3c1efb56
Rexcy Mosses <50251783+rafards@users.noreply.github.com>
2026-03-07 19:38:55 +0700

    Update bracketSystem.js

--- src/systems/bracketSystem.js
+++ src/systems/bracketSystem.js
@@ -1,16 +1,38 @@
-const bracketData = {
+const { raceState } = require("../data/raceState")
 
- step: "race_name",
+function shuffle(array){
+ return array.sort(()=>Math.random()-0.5)
+}
 
- name: null,
- type: null,
- price: null,
- lap: null,
- slots: null,
- time: null,
+function generateMatches(players){
 
- players: []
+ const matches = []
+
+ for(let i=0;i<players.length;i+=2){
+
+  matches.push({
+   player1: players[i],
+   player2: players[i+1],
+   winner:null
+  })
+
+ }
+
+ return matches
+}
+
+async function generateBracket(interaction){
+
+ const shuffled = shuffle([...raceState.players])
+
+ raceState.matches = generateMatches(shuffled)
+
+ raceState.currentRound = 1
+
+ const { sendBracketPanel } = require("../utils/bracketPanelBuilder")
+
+ await sendBracketPanel(interaction)
 
 }
 
-module.exports = { bracketData }
+module.exports = { generateBracket }
