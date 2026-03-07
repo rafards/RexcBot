36ceb9c48acf641f0de0fa6adf34b63134192ea2
Rexcy Mosses <50251783+rafards@users.noreply.github.com>
2026-03-07 11:34:21 +0700

    Create config.js

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+ token: process.env.TOKEN,
+
+ btRole: process.env.BT_ROLE_ID,
+
+ setupChannel: process.env.SETUP_CHANNEL_ID,
+
+ infoRaceChannel: process.env.INFO_RACE_CHANNEL_ID
+
+}
