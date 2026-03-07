50bb663bba93329a19af072275322eabd25a2c89
Rexcy Mosses <50251783+rafards@users.noreply.github.com>
2026-03-07 12:14:24 +0700

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,20 +1,30 @@
 const { client } = require("../index")
 
 const { registrationButton } = require("../interactions/buttons/registrationButton")
+const { bracketButtons } = require("../interactions/buttons/bracketButtons")
 const { handleBracketModals } = require("../interactions/modals/bracketModals")
 
 client.on("interactionCreate", async interaction => {
 
+ // ===============================
+ // BUTTON HANDLER
+ // ===============================
+
  if(interaction.isButton()){
 
-  if(interaction.customId === "set_registration"){
-   registrationButton(interaction)
-  }
+  await bracketButtons(interaction)
+  await registrationButton(interaction)
 
  }
 
+ // ===============================
+ // MODAL HANDLER
+ // ===============================
+
  if(interaction.isModalSubmit()){
-  handleBracketModals(interaction)
+
+  await handleBracketModals(interaction)
+
  }
 
 })
