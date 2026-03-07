2f4fb99e8bc59582c5e55309c2079656efa7867a
Rexcy Mosses <50251783+rafards@users.noreply.github.com>
2026-03-07 12:29:41 +0700

    Create raceState.js

---
+++ src/data/raceState.js
@@ -0,0 +1,17 @@
+const raceState = {
+
+ raceName: null,
+ raceType: null,
+ racePrice: 0,
+
+ lap: null,
+ slot: null,
+ time: null,
+
+ players: [],
+
+ deployed: false
+
+}
+
+module.exports = { raceState }
