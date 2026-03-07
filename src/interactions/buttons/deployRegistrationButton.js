91fdd625cf4fbb0f5e857ff6a4538531a0d5457f
Rexcy Mosses <50251783+rafards@users.noreply.github.com>
2026-03-07 17:29:45 +0700

    Update deployRegistrationButton.js

--- src/interactions/buttons/deployRegistrationButton.js
+++ src/interactions/buttons/deployRegistrationButton.js
@@ -7,7 +7,58 @@ async function deployRegistrationButton(interaction){
 
  await interaction.deferUpdate()
 
+ const playerChannel = interaction.guild.channels.cache.find(
+  c => c.name === "info-race"
+ )
+
+ if(!playerChannel){
+  return
+ }
+
  raceState.registrationOpen = true
+
+ // ================= PLAYER PANEL =================
+
+ const playerEmbed = new EmbedBuilder()
+  .setTitle(`🏁 ${raceState.raceName}`)
+  .setDescription(`Players\n0 / ${raceState.slot}`)
+
+ const joinButton = new ButtonBuilder()
+  .setCustomId("join_race")
+  .setLabel("Join")
+  .setStyle(ButtonStyle.Success)
+
+ const leaveButton = new ButtonBuilder()
+  .setCustomId("leave_race")
+  .setLabel("Leave")
+  .setStyle(ButtonStyle.Danger)
+
+ const row = new ActionRowBuilder().addComponents(joinButton,leaveButton)
+
+ const playerPanel = await playerChannel.send({
+  embeds:[playerEmbed],
+  components:[row]
+ })
+
+ raceState.playerPanelId = playerPanel.id
+
+ // ================= ADMIN PANEL =================
+
+ let list=""
+
+ for(let i=1;i<=raceState.slot;i++){
+  list += `${i}.\n`
+ }
+
+ const adminEmbed = new EmbedBuilder()
+  .setTitle("📋 Player List")
+  .setDescription(list)
+
+ const adminPanel = await interaction.channel.send({
+  embeds:[adminEmbed]
+ })
+
+ raceState.adminListPanelId = adminPanel.id
 
 }
 
