157c63113dfc51dd9a4bb2018c9f84643086d274
Rexcy Mosses <50251783+rafards@users.noreply.github.com>
2026-03-07 11:47:36 +0700

    Update deployBracket.js

--- src/commands/bracket/deployBracket.js
+++ src/commands/bracket/deployBracket.js
@@ -1,3 +1,4 @@
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js")
 const { createBracketEmbed } = require("../../utils/embeds")
 const { bracketData } = require("../../systems/bracketSystem")
 
@@ -5,8 +6,18 @@ async function deployBracket(message){
 
  const embed = createBracketEmbed(bracketData)
 
+ const row = new ActionRowBuilder().addComponents(
+
+  new ButtonBuilder()
+   .setCustomId("set_race_name")
+   .setLabel("Set Race Name")
+   .setStyle(ButtonStyle.Primary)
+
+ )
+
  await message.channel.send({
-  embeds: [embed]
+  embeds:[embed],
+  components:[row]
  })
 
 }
