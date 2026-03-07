46e809894d7737e7b7f68b6b1ef31e6dd2df8beb
Rexcy Mosses <50251783+rafards@users.noreply.github.com>
2026-03-07 15:00:45 +0700

    Update bracketGenerator.js

--- src/utils/bracketGenerator.js
+++ src/utils/bracketGenerator.js
@@ -2,8 +2,7 @@ function shuffle(array){
 
  for(let i=array.length-1;i>0;i--){
 
-  const j = Math.floor(Math.random()*(i+1))
-
+  const j=Math.floor(Math.random()*(i+1))
   ;[array[i],array[j]]=[array[j],array[i]]
 
  }
@@ -18,22 +17,34 @@ function generateBracket(players){
 
  const matches = []
 
+ let oddPlayer = null
+
+ if(shuffled.length % 2 !== 0){
+
+  oddPlayer = shuffled.pop()
+
+ }
+
  for(let i=0;i<shuffled.length;i+=2){
 
-  const player1 = shuffled[i]
-  const player2 = shuffled[i+1] || null
-
   matches.push({
-   player1,
-   player2,
+
+   player1: shuffled[i],
+   player2: shuffled[i+1],
+
    winner:null,
    loser:null,
+
    round:1
+
   })
 
  }
 
- return matches
+ return {
+  matches,
+  oddPlayer
+ }
 
 }
 
