8b68a39bb0c61534a4e61e0652abc1d41fe24849
Rexcy Mosses <50251783+rafards@users.noreply.github.com>
2026-03-07 17:26:56 +0700

    Update updateRegistrationPanels.js

--- src/utils/updateRegistrationPanels.js
+++ src/utils/updateRegistrationPanels.js
@@ -1,60 +1,35 @@
-const { EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js")
+const { EmbedBuilder } = require("discord.js")
 const { raceState } = require("../data/raceState")
 
 async function updateRegistrationPanels(interaction){
 
  const guild = interaction.guild
 
- // ======================
- // PLAYER PANEL
- // ======================
-
  const playerChannel = guild.channels.cache.find(c => c.name === "info-race")
+ const adminChannel = guild.channels.cache.find(c => c.name === "setup-bot")
 
  const playerPanel = await playerChannel.messages.fetch(raceState.playerPanelId)
+ const adminPanel = await adminChannel.messages.fetch(raceState.adminListPanelId)
 
  const playerEmbed = new EmbedBuilder()
   .setTitle(`🏁 ${raceState.raceName}`)
-  .setDescription(`Players\n${raceState.players.length} / ${raceState.slot}`)
-
- const joinButton = new ButtonBuilder()
-  .setCustomId("join_race")
-  .setLabel("Join")
-  .setStyle(ButtonStyle.Success)
-
- const leaveButton = new ButtonBuilder()
-  .setCustomId("leave_race")
-  .setLabel("Leave")
-  .setStyle(ButtonStyle.Danger)
-
- const row = new ActionRowBuilder().addComponents(joinButton,leaveButton)
+  .setDescription(`Players\n${raceState.players.length}/${raceState.slot}`)
 
  await playerPanel.edit({
-  embeds:[playerEmbed],
-  components:[row]
+  embeds:[playerEmbed]
  })
 
- // ======================
- // ADMIN PANEL
- // ======================
-
- const adminChannel = guild.channels.cache.find(c => c.name === "setup-bot")
-
- const adminPanel = await adminChannel.messages.fetch(raceState.adminListPanelId)
-
- let list = ""
+ let text=""
 
  raceState.players.forEach((p,i)=>{
-  list += `${i+1}. ${p.ign}\n`
+  text += `${i+1}. ${p.ign}\n`
  })
 
- for(let i=raceState.players.length+1;i<=raceState.slot;i++){
-  list += `${i}.\n`
- }
+ if(text === "") text="No players yet"
 
  const adminEmbed = new EmbedBuilder()
   .setTitle("📋 Player List")
-  .setDescription(list)
+  .setDescription(text)
 
  await adminPanel.edit({
   embeds:[adminEmbed]
