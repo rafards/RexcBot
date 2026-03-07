b022d4f206a1533cf4d25c1a8288f9d1561cb951
Rexcy Mosses <50251783+rafards@users.noreply.github.com>
2026-03-07 13:08:11 +0700

    Update deployBracket.js

--- src/commands/bracket/deployBracket.js
+++ src/commands/bracket/deployBracket.js
@@ -1,20 +1,19 @@
 const { createBracketEmbed } = require("../../utils/embeds")
-const { bracketData } = require("../../systems/bracketSystem")
 const { getSetupButton } = require("../../utils/bracketButtons")
 const { raceState } = require("../../data/raceState")
 
 async function deployBracket(message){
 
- const embed = createBracketEmbed(bracketData)
+ const embed = createBracketEmbed(raceState)
 
  const row = getSetupButton("race_name")
 
  const msg = await message.channel.send({
-  embeds:[embed],
-  components:[row]
+  embeds: [embed],
+  components: [row]
  })
 
- // simpan panel
+ // simpan panel message supaya bisa diedit
  raceState.panelMessageId = msg.id
  raceState.panelChannelId = message.channel.id
 
