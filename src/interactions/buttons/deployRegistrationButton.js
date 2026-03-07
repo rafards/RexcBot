fddd7a6f85811a8333c7f939318573f9e2bcff10
Rexcy Mosses <50251783+rafards@users.noreply.github.com>
2026-03-07 16:12:45 +0700

    Update deployRegistrationButton.js

--- src/interactions/buttons/deployRegistrationButton.js
+++ src/interactions/buttons/deployRegistrationButton.js
@@ -18,53 +18,6 @@ async function deployRegistrationButton(interaction){
 
  raceState.registrationOpen = true
 
- // ======================
- // UPDATE PLAYER PANEL
- // ======================
-
- const playerEmbed = new EmbedBuilder()
-  .setTitle(`🏁 ${raceState.raceName}`)
-  .setDescription(`Players\n${raceState.players.length} / ${raceState.slot}`)
-
- const joinButton = new ButtonBuilder()
-  .setCustomId("join_race")
-  .setLabel("Join")
-  .setStyle(ButtonStyle.Success)
-
- const leaveButton = new ButtonBuilder()
-  .setCustomId("leave_race")
-  .setLabel("Leave")
-  .setStyle(ButtonStyle.Danger)
-
- const row = new ActionRowBuilder().addComponents(joinButton,leaveButton)
-
- await playerPanel.edit({
-  embeds:[playerEmbed],
-  components:[row]
- })
-
- // ======================
- // UPDATE ADMIN PANEL
- // ======================
-
- const adminPanel = await interaction.channel.messages.fetch(raceState.adminListPanelId)
-
- let list = ""
-
- raceState.players.forEach((p,i)=>{
-  list += `${i+1}. ${p.ign}\n`
- })
-
- if(list === "") list = "No players yet"
-
- const adminEmbed = new EmbedBuilder()
-  .setTitle("📋 Player List")
-  .setDescription(list)
-
- await adminPanel.edit({
-  embeds:[adminEmbed]
- })
-
 }
 
 module.exports = { deployRegistrationButton }
