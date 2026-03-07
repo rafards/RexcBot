f5d2f420ea104a0d92b387dd2d055f403ef420d8
Rexcy Mosses <50251783+rafards@users.noreply.github.com>
2026-03-07 12:08:10 +0700

    Update bracketButtons.js

--- src/interactions/buttons/bracketButtons.js
+++ src/interactions/buttons/bracketButtons.js
@@ -1,6 +1,12 @@
-const { ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = require("discord.js")
+const {
+ ModalBuilder,
+ TextInputBuilder,
+ TextInputStyle,
+ ActionRowBuilder,
+ StringSelectMenuBuilder
+} = require("discord.js")
 
-async function handleBracketButtons(interaction){
+async function bracketButtons(interaction){
 
  if(interaction.customId === "set_race_name"){
 
@@ -22,6 +28,54 @@ async function handleBracketButtons(interaction){
 
  }
 
+ if(interaction.customId === "set_registration"){
+
+  const modal = new ModalBuilder()
+   .setCustomId("registration_modal")
+   .setTitle("Registration Setup")
+
+  const typeInput = new TextInputBuilder()
+   .setCustomId("race_type_input")
+   .setLabel("Type (Gratis / Berbayar)")
+   .setStyle(TextInputStyle.Short)
+   .setRequired(true)
+
+  const priceInput = new TextInputBuilder()
+   .setCustomId("race_price_input")
+   .setLabel("Price (Isi 0 jika Gratis)")
+   .setStyle(TextInputStyle.Short)
+   .setRequired(true)
+
+  const row1 = new ActionRowBuilder().addComponents(typeInput)
+  const row2 = new ActionRowBuilder().addComponents(priceInput)
+
+  modal.addComponents(row1,row2)
+
+  await interaction.showModal(modal)
+
+ }
+
+ if(interaction.customId === "set_lap"){
+
+  const menu = new StringSelectMenuBuilder()
+   .setCustomId("lap_select")
+   .setPlaceholder("Select Lap")
+   .addOptions([
+    { label:"1 Lap", value:"1" },
+    { label:"2 Lap", value:"2" },
+    { label:"3 Lap", value:"3" }
+   ])
+
+  const row = new ActionRowBuilder().addComponents(menu)
+
+  await interaction.reply({
+   content:"Select race lap",
+   components:[row],
+   ephemeral:true
+  })
+
+ }
+
 }
 
-module.exports = { handleBracketButtons }
+module.exports = { bracketButtons }
