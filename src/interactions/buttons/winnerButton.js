2e50786325eeabb1ce1e0d67a334de79eace375d
Rexcy Mosses <50251783+rafards@users.noreply.github.com>
2026-03-07 12:53:39 +0700

    Update winnerButton.js

--- src/interactions/buttons/winnerButton.js
+++ src/interactions/buttons/winnerButton.js
@@ -22,9 +22,44 @@ async function winnerButton(interaction){
 
  match.winner = winner
 
- await interaction.reply({
-  content:`🏆 Winner: **${winner.name}**`
+ // cek apakah semua match selesai
+
+const finished = raceState.matches.every(m => m.winner)
+
+if(finished){
+
+ const winners = raceState.matches.map(m => m.winner)
+
+ // jika tinggal 1 → tournament selesai
+ if(winners.length === 1){
+
+  return interaction.followUp({
+   content:`🏆 TOURNAMENT WINNER: **${winners[0].name}**`
+  })
+
+ }
+
+ // generate round berikutnya
+ raceState.matches = generateNextRound(raceState.matches)
+
+ raceState.currentRound++
+
+ let text = ""
+
+ raceState.matches.forEach((m,i)=>{
+
+  text += `Match ${i+1}\n`
+  text += `${m.player1.name} vs ${m.player2.name}\n\n`
+
  })
+
+ await interaction.followUp({
+
+  content:`⚔️ ROUND ${raceState.currentRound}\n\n${text}`
+
+ })
+
+}
 
 }
 
