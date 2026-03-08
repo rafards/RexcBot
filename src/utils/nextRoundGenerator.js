cf2b04b6b941e257f0418fe7b977c8d5dad2c81e
Rexcy Mosses <50251783+rafards@users.noreply.github.com>
2026-03-08 22:30:41 +0700

    Update nextRoundGenerator.js

--- src/utils/nextRoundGenerator.js
+++ src/utils/nextRoundGenerator.js
@@ -1,19 +1,20 @@
 function generateNextRound(matches){
 
- const winners = matches.map(m=>m.winner).filter(Boolean)
+ const winners = matches
+  .map(m=>m.winner)
+  .filter(Boolean)
 
  const next=[]
 
  for(let i=0;i<winners.length;i+=2){
 
+  if(!winners[i+1]) break
+
   next.push({
-
    player1:winners[i],
-   player2:winners[i+1] || null,
-
+   player2:winners[i+1],
    winner:null,
    loser:null
-
   })
 
  }
