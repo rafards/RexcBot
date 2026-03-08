90ea38b85e6ea923c1b9c46bb1ed1331d4ee3ec7
Rexcy Mosses <50251783+rafards@users.noreply.github.com>
2026-03-08 21:37:03 +0700

    Update bracketButtons.js

--- src/utils/bracketButtons.js
+++ src/utils/bracketButtons.js
@@ -4,59 +4,31 @@ function getSetupButton(step){
 
  let button
 
- if(step === "race_name"){
+ // ===============================
+ // SETUP RACE
+ // ===============================
+
+ if(step === "setup_race"){
 
   button = new ButtonBuilder()
-   .setCustomId("set_race_name")
-   .setLabel("Set Race Name")
+   .setCustomId("setup_race")
+   .setLabel("Setup Race")
    .setStyle(ButtonStyle.Primary)
 
  }
 
- else if(step === "registration"){
-
-  button = new ButtonBuilder()
-   .setCustomId("set_registration")
-   .setLabel("Set Registration")
-   .setStyle(ButtonStyle.Primary)
-
- }
-
- else if(step === "lap"){
-
-  button = new ButtonBuilder()
-   .setCustomId("set_lap")
-   .setLabel("Set Lap")
-   .setStyle(ButtonStyle.Primary)
-
- }
-
- else if(step === "slot"){
-
-  button = new ButtonBuilder()
-   .setCustomId("set_slot")
-   .setLabel("Set Player Slot")
-   .setStyle(ButtonStyle.Primary)
-
- }
-
- else if(step === "race_time"){
-
-  button = new ButtonBuilder()
-   .setCustomId("set_race_time")
-   .setLabel("Set Race Time")
-   .setStyle(ButtonStyle.Primary)
-
- }
+ // ===============================
+ // DEPLOY REGISTRATION
+ // ===============================
 
  else if(step === "deploy"){
 
- button = new ButtonBuilder()
-  .setCustomId("deploy_registration")
-  .setLabel("Deploy Registration")
-  .setStyle(ButtonStyle.Success)
+  button = new ButtonBuilder()
+   .setCustomId("deploy_registration")
+   .setLabel("Deploy Registration")
+   .setStyle(ButtonStyle.Success)
 
-}
+ }
 
  return new ActionRowBuilder().addComponents(button)
 
