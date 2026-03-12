3cc199e3d43980e0b23ee80f6b1f801f47049e58
Rexcy Mosses <50251783+rafards@users.noreply.github.com>
2026-03-13 06:21:47 +0700

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -132,27 +132,6 @@ client.on("interactionCreate", async interaction => {
   return winnerButton(interaction)
  }
 
- if(interaction.customId.startsWith("lucky_")){
-
-  const index = parseInt(interaction.customId.split("_")[1])
-
-  const lucky = raceState.luckyLoserCandidates[index]
-
-  raceState.matches.push({
-   player1: raceState.waitingPlayer,
-   player2: lucky,
-   winner:null,
-   loser:null
-  })
-
-  raceState.luckyLoserMode = false
-  raceState.luckyLoserCandidates = []
-
-  await interaction.deferUpdate()
-
-  return updateBracketPanel(interaction.client)
- }
-
  return bracketButtons(interaction)
  }
 
