e58697d65bf535cb94cd9c3488d2a7338092306f
Rexcy Mosses <50251783+rafards@users.noreply.github.com>
2026-03-13 18:45:49 +0700

    Update deployBracket.js

--- src/commands/bracket/deployBracket.js
+++ src/commands/bracket/deployBracket.js
@@ -1,8 +1,25 @@
 const { createBracketEmbed } = require("../../utils/embeds")
 const { getSetupButton } = require("../../utils/bracketButtons")
 const { raceState } = require("../../data/raceState")
+const config = require("../../config")
 
 async function deployBracket(message){
+
+ // ===============================
+ // CHANNEL RESTRICTION
+ // ===============================
+
+ if(message.channel.id !== config.setupChannel){
+  return
+ }
+
+ // ===============================
+ // ROLE RESTRICTION
+ // ===============================
+
+ if(!message.member.roles.cache.has(config.btRole)){
+  return message.reply("❌ Kamu tidak memiliki izin untuk command ini.")
+ }
 
  if(raceState.panelMessageId){
   await message.delete().catch(()=>{})
