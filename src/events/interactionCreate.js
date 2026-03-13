e0738c4e63f16ad8291a9fdd9e9960a8f8870e3a
Rexcy Mosses <50251783+rafards@users.noreply.github.com>
2026-03-14 06:13:31 +0700

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -85,83 +85,15 @@ if(interaction.isStringSelectMenu()){
   return fillTestPlayersButton(interaction)
  }
 
-  await selectChampionButton(interaction)
-
  if(interaction.customId === "reset_tournament"){
   return resetTournamentButton(interaction)
  }
 
- if(interaction.customId.startsWith("select_p1_")){
-
- const index = parseInt(interaction.customId.split("_")[2])
- const player = raceState.roundRobinPlayers[index]
-
- raceState.p1 = player
-
- const remaining = raceState.roundRobinPlayers.filter(p=>p!==player)
-
- const buttons = remaining.map((p,i)=>({
-  type:2,
-  style:1,
-  label:p.ign,
-  custom_id:`select_p2_${i}`
- }))
-
- await interaction.update({
-  embeds:[{
-   title:"Select 2nd Place (P2)"
-  }],
-  components:[{
-   type:1,
-   components:buttons
-  }]
- })
-
- return
- }
-
- if(interaction.customId.startsWith("select_p2_")){
-
-  const index = parseInt(interaction.customId.split("_")[2])
-
-  const remaining = raceState.roundRobinPlayers.filter(p=>p!==raceState.p1)
-
-  raceState.p2 = remaining[index]
-
-  raceState.p3 = remaining.find(p=>p!==raceState.p2)
-
-  const playerChannel = await interaction.client.channels.fetch(raceState.playerPanelChannelId)
-
-  await playerChannel.send({
-   embeds:[{
-    title:"🏆 TOURNAMENT RESULT",
-    description:
-   `🥇 ${raceState.p1.ign}
-   🥈 ${raceState.p2.ign}
-   🥉 ${raceState.p3.ign}`,
-    color:0xFFD700
-   }]
-  })
-
-  await interaction.update({
-   embeds:[{
-    title:"Result Locked"
-   }],
-   components:[]
-  })
-
-  const resetButton = new ButtonBuilder()
-  .setCustomId("reset_tournament")
-  .setLabel("Reset Tournament")
-  .setStyle(ButtonStyle.Danger)
- 
-  const row = new ActionRowBuilder().addComponents(resetButton)
-
-  await interaction.channel.send({
-   components:[row]
-  })
-
-  return
+ if(
+  interaction.customId.startsWith("select_p1_") ||
+  interaction.customId.startsWith("select_p2_")
+ ){
+  return selectChampionButton(interaction)
  }
 
  if(interaction.customId.startsWith("winner_")){
